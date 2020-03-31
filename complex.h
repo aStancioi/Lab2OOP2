4fa49e376cf3292a85d6caa5d9b1eb7c15b54b3e
SMara99 <61970933+SMara99@users.noreply.github.com>
2020-03-31 14:25:31 +0300

    Update complex.h

--- complex.h
+++ complex.h
@@ -17,6 +17,19 @@ public:
 	double getreal() const;
 	//returns the imaginary part of the number
 	double getimg() const;
+	
+	//setter
+	void set_real(double real_value);
+	void set_img(double img_value);
+	
+	//operatii aritmetice
+	complex add(complex other);
+	complex mul(complex other);
+	complex quot(complex other);
+	
+	//absoluten Betrag
+	double abs();
+	
 	//returns a complex number
 	void show_compl();
 	//displays the exponential form of a complex number
