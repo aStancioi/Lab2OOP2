7cd5355f081c68823d65a1580ed0e3cf8134928b
SMara99 <61970933+SMara99@users.noreply.github.com>
2020-03-31 14:48:39 +0300

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -20,6 +20,59 @@ double Complex::getimg() const
 {
 	return imaginar;
 }
+//setter real
+void complex::set_real(double real_value) {
+	// ocuparea partii reale
+	// param : a_value
+	real = real_value;
+}
+//setter imaginar
+void complex::set_img(double img_value) {
+	// ocuparea partii imaginare
+	// param : img_value
+	imaginar = img_value;
+}
+
+complex complex::add(complex other) {
+	// returneaza suma a doua numere complexe
+	complex auxiliar;
+
+	auxiliar.set_real(this->getreal() + other.getreal());
+	auxiliar.set_img(this->getimg() + other.getimg());
+
+	return auxiliar;
+}
+
+complex complex::mul(complex other) {
+	// returneaza inmultirea a doua numere complexe
+	complex auxiliar;
+
+	auxiliar.set_real(this->getreal() * other.getreal() - this->getimg() * other.getimg());
+	auxiliar.set_img(this->getimg() * other.getimg() + this->getreal() * other.getimg());
+
+	return auxiliar;
+}
+
+complex complex::quot(complex other) {
+	// returneaza impartirea a doua numere complexe
+	complex auxiliar;
+	double div = this->getimg() * this->getimg() + other.getimg() * other.getimg();
+	auxiliar.set_real(
+		(this->getreal() * other.getreal() + this->getimg() * other.getimg()) / div);
+	auxiliar.set_img(
+		(this->getimg() * other.getimg() - this->getreal() * other.getimg()) / div);
+	
+	return auxiliar;
+}
+
+double complex::abs() {
+	// returneaza modulul unui numar complex
+	double result;
+
+	result = sqrt(this->getreal() * this->getreal() + this->getimg() * this->getimg());
+
+	return result;
+}
 
 void Complex::show_compl()
 {
